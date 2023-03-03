5df49bc07268bfa6ad53b20635d8b0fad354bcb0
ranky akab <rankyakab@gmail.com>
2023-03-03 15:15:00 +0100

    my second

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ export default function App() {
         <ThemeProvider>
           <ThemeSettings>
             <ThemeLocalization>
+            
               <SnackbarProvider>
                 <Router />
               </SnackbarProvider>
